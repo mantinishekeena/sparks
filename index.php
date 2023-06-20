71bd0f5b76b22cfa4b139b25576fb4b7b87aba64
mantinishekeena <113205004+mantinishekeena@users.noreply.github.com>
2023-06-20 13:45:19 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- navbar starts here  -->
+    <nav class="navbar">
+        <a href="index.php">Bank of India</a>
+    </nav>
+    <!-- navbar ends here  -->
+    <div class="container">
+        <div class="content">
+            <h1>Welcome to Bank of India</h1>
+            <p>Pay Hassle Free</p>
+            <br>
+            <a class="transfer" href="customer.php">Customers</a>
+        </div>
+        <div class="image">
+            <img src="transaction.svg" alt="money-transfer">
+        </div>
+    </div>
+</body>
+</html>
